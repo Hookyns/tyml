fbdb255f4221dcc4f94c1fb3adc499f7bf4a4246
Hookyns <hookcze@gmail.com>
2021-03-22 00:01:21 +0100

    Demo app - testing Console output

---
+++ RJDev.Tyml.Core.Demo.App/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using RJDev.Outputter.Sinks.Console;
+using RJDev.Outputter.Sinks.Console.Themes;
+using RJDev.Tyml.Core.Tasks;
+using RJDev.Tyml.Core.Tests;
+using RJDev.Tyml.Core.Tests.TestTasks.Cmd;
+using RJDev.Tyml.Core.Tests.TestTasks.LongDelay;
+
+namespace RJDev.Tyml.Core.Demo.App
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			IServiceProvider serviceProvider = GetServiceProvider();
+			TymlContext context = GetContext();
+			TymlExecutor executor = serviceProvider.GetRequiredService<TymlExecutor>();
+			
+			using ConsoleSink sink = new ConsoleSink(
+				new ConsoleSinkOptions(ColorTheme.DarkConsole)
+			);
+
+			string yaml = @"
+steps:
+  - task: Cmd
+    displayName: 'Echo the most important message'
+    inputs:
+      Script: 'echo Hello World!'
+";
+
+			var results = await executor.Execute(context, yaml);
+
+			foreach (TaskOutput taskOutput in results)
+			{
+				await taskOutput.Output.Pipe(sink);
+				// this.testOutputHelper.WriteLine(taskOutput.Output);
+			}
+		}
+		
+		/// <summary>
+		/// Return context istance
+		/// </summary>
+		/// <returns></returns>
+		protected static TymlContext GetContext()
+		{
+			return new TymlContextBuilder()
+				.AddTasks(typeof(CmdTask), typeof(LongDelayTask))
+				.UseWorkingDirectory(Path.Combine(Directory.GetCurrentDirectory(), "work-dir"))
+				.WithBaseVariables(new Dictionary<string, object>()
+				{
+					{"foo", 5},
+					{"bar", "string"},
+					{"lipsum", "Lorem Ipsum dolor sit amet."},
+					{"assemblyVersion", typeof(TestBase).Assembly.GetName().Version?.ToString() ?? string.Empty},
+				})
+				.Build();
+		}
+
+		/// <summary>
+		/// Prepare IServiceProvider
+		/// </summary>
+		/// <returns></returns>
+		protected static IServiceProvider GetServiceProvider()
+		{
+			ServiceCollection collection = new();
+			collection.AddSingleton<TymlExecutor>();
+
+			collection.AddTransient<CmdTask>();
+			collection.AddTransient<LongDelayTask>();
+
+			ServiceProvider provider = collection.BuildServiceProvider();
+			return provider;
+		}
+	}
+}
